6342b7d25b080aa30ee8d30033e91f272a63c058
Dildar-Ali-1860 <107632057+Dildar-Ali-1860@users.noreply.github.com>
2022-07-14 21:07:14 +0500

    Self Built-in Modules

---
+++ mymath.py
@@ -0,0 +1,36 @@
+def addition(*nums):
+    """
+    Returns sum of numbers provided.
+    returns error message if no number is provided"""
+    if len(nums)==0:
+        return "atleast two arguments required"
+    else:
+        return sum(nums)
+
+def exponent(num,exp):
+    """
+    returns Exponents of provided number
+    First parameter is numbers.
+    Second parameter is exponent.
+    """
+    return num**exp
+
+def len(para):
+    """
+    Return length of paragraph"""
+    return len(para)
+
+def sent(para):
+    """
+    Return length of paragraph"""
+    a=para.split(". ")
+    return len(a)
+
+def words(para):
+    """
+    Return length of paragraph"""
+    a=para.split(" ")
+    return len(a)
+
+def 
+    
